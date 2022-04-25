ad204a41911f1ab5dfcce892f68720d660d79d67
sramkellawan <sramkellawan@gmail.com>
2022-04-25 19:52:50 -0400

    Checkout Form

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react'
 import formatCurrency from '../util';
 
 export default class Cart extends Component {
+    constructor(props){
+     super(props);
+     this.state={
+         name: "",
+         address: "",
+         email: "",
+         showCheckout: false};
+    }
+    handleInput = (e) =>{
+        this.setState({[e.target.name]: e.target.value})
+    }
+    createOrder = (e) => {
+        e.preventDefault();
+        const order = {
+          name: this.state.name,
+          email: this.state.email,
+          address: this.state.address,
+          cartItems: this.props.cartItems,
+        };
+        this.props.createOrder(order);
+    }
   render() {
     const {cartItems} = this.props;
     return (
@@ -27,7 +48,8 @@ export default class Cart extends Component {
                       {formatCurrency(item.price)} x {item.count}{" "}
                       <button
                         className="button"
-                        onClick={() => this.props.removeFromCart(item)}>
+                        onClick={() => this.props.removeFromCart(item)}
+                      >
                         Remove
                       </button>
                     </div>
@@ -37,18 +59,63 @@ export default class Cart extends Component {
             </ul>
           </div>
           {cartItems.length !== 0 && (
-            <div className="cart">
-              <div className="total">
-                <div>
-                    
-                  Total:{" "}
-                  {formatCurrency(
-                    cartItems.reduce((a, c) => a + c.price * c.count, 0)
-                  )} 
-
+            <div>
+              <div className="cart">
+                <div className="total">
+                  <div>
+                    Total:{" "}
+                    {formatCurrency(
+                      cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                    )}
+                  </div>
+                  <button
+                    onClick={() => {
+                      this.setState({ showCheckout: true });
+                    }}
+                    className="button primary"
+                  >
+                    Proceed
+                  </button>
                 </div>
-                <button className="button primary">Proceed</button>
               </div>
+              {this.state.showCheckout && (
+                <div className="cart">
+                  <form onSubmit={this.createOrder}>
+                    <ul className="form-container">
+                      <li>
+                        <label>Email</label>
+                        <input
+                          name="email"
+                          type="email"
+                          required
+                          onChange={this.handleInput}
+                        ></input>
+                      </li>
+                      <li>
+                        <label>Name</label>
+                        <input
+                          name="name"
+                          type="text"
+                          required
+                          onChange={this.handleInput}
+                        ></input>
+                      </li>
+                      <li>
+                        <label>Address</label>
+                        <input
+                          name="address"
+                          type="text"
+                          required
+                          onChange={this.handleInput}
+                        ></input>
+                      </li>
+                      <li>
+                          <button className="button primary" type="submit">Checkout</button>
+                      </li>
+                    </ul>
+                  </form>
+                </div>
+              )}
             </div>
           )}
         </div>
