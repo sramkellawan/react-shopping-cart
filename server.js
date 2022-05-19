64ad5fa5a8312498e1b56e05527ddb9dac7bf4e1
sramkellawan <sramkellawan@gmail.com>
2022-05-18 22:02:53 -0400

    create order

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ mongoose.connect(
   }
 );
 
-
 const Product = mongoose.model(
   "products",
   new mongoose.Schema({
@@ -35,22 +34,61 @@ const Product = mongoose.model(
   })
 );
 
-app.get("/api/products", async (req, res) =>{
-    const products = await Product.find({});
-    res.send(products);
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
 });
 
-app.post("/api/products", async (req, res)=>{
-    const newProduct = new Product(req.body);
-    const savedProduct = await newProduct.save();
-    res.send(savedProduct);
+app.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
 });
 
 app.delete("/api/products/:id", async (req, res) => {
-  const deletedProduct = await Product.findByIdAndDelete(req.params.id)    ;
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
   res.send(deletedProduct);
 });
 
+const Order = mongoose.model(
+  "order",
+  new mongoose.Schema({
+    _id: {
+      type: String,
+      default: shortid.generate,
+    },
+    email: String,
+    name: String,
+    address: String,
+    total: Number,
+    cartItems: [
+      {
+        _id: String,
+        title: String,
+        price: Number,
+        count: Number,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+  )
+);
+app.post("/api/orders", async (req, res) => {
+  if (
+    !req.body.name ||
+    !req.body.email ||
+    !req.body.address ||
+    !req.body.total || 
+    !req.body.cartItems
+   ) {
+    return res.send({ message: "Data is required."});
+  }
+  const order = await Order(req.body).save();
+  res.send(order);
+});
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log("serve at http://localhost:5000"));
-
